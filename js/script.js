78fa3c05a661485283712b20d000340fdc16a49c
riccardo-guido <guidoriccardo02@gmail.com>
2025-04-28 17:06:56 +0200

    modified-html-and-css-without-bootstrap

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-//# DOM ELEMENTS
+axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
+  const posts = response.data;
+  console.log(posts);
+
+  posts.forEach((post) => {
+    //...
+  });
+});
+
+const generatePostCard = (post) => {
+  const cardHtml = `
+        
+        `;
+};
