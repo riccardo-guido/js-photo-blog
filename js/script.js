26aab18b52d23a43c454f7c84eb90f02b727a5ea
riccardo-guido <guidoriccardo02@gmail.com>
2025-04-28 17:23:02 +0200

    cards-generated-via-javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
+const postsContainer = document.getElementById("posts-container");
+
 axios.get("https://lanciweb.github.io/demo/api/pictures/").then((response) => {
   const posts = response.data;
-  console.log(posts);
 
+  let cardsHtml = "";
   posts.forEach((post) => {
-    //...
+    cardsHtml += generatePostCard(post);
   });
+  postsContainer.innerHTML += cardsHtml;
 });
 
 const generatePostCard = (post) => {
   const cardHtml = `
-        
+        <div class="card">
+            <div class="card-media">
+              <img src="${post.url}" alt="${post.title}" />
+            </div>
+            <div class="card-content">
+            <span>
+             ${post.date}
+            </span>
+            <span>
+             ${post.title}
+            </span>
+           
+            </div>
+          </div>
         `;
+  return cardHtml;
 };
